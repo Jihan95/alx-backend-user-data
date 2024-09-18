bf3bffc184180735d68c6468be85b89adec0c20d
Jihan <Jihan>
2024-09-18 04:29:33 +0000

    task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@ Basic auth
 """
 from api.v1.auth.auth import Auth
 from typing import Union
+import base64
 
 
 class BasicAuth(Auth):
@@ -22,3 +23,18 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header.split(" ", 1)[1].strip()
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> Union[None, str]:
+        """
+        decode_base64_authorization_header
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode('utf-8')
+        except Exception:
+            return None
