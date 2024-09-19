7b3049a3964cdb5441edf7c52aaad4613098b076
Jihan <Jihan>
2024-09-19 15:28:32 +0000

    5. Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -54,10 +54,17 @@ def check_auth():
     method to check authentication before request
     """
     if auth is not None:
-        ex = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+        ex = [
+                '/api/v1/status/',
+                '/api/v1/unauthorized/',
+                '/api/v1/forbidden/',
+                '/api/v1/auth_session/login/'
+                ]
         if not auth.require_auth(request.path, ex):
             return
-        if auth.authorization_header(request) is None:
+        is_auth = auth.authorization_header(request)
+        has_cookie = auth.session_cookie(request)
+        if is_auth is None and has_cookie is None:
             abort(401)
         if auth.current_user(request) is None:
             abort(403)
