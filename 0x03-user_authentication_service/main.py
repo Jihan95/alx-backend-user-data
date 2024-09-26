9e61c94d187f84f7fcb15afbf88e81463496ad5e
jihan ahmed <jihanahmed41@gmail.com>
2024-09-26 19:46:45 +0000

    20

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -1,14 +1,80 @@
 #!/usr/bin/env python3
 """
-Main file
+End-to-end integration test
 """
-from auth import Auth
+import requests
 
-email = 'bob@bob.com'
-password = 'MyPwdOfBob'
-auth = Auth()
 
-auth.register_user(email, password)
+def register_user(email: str, password: str) -> None:
+    """
+    test register user method
+    """
+    pass
 
-print(auth.create_session(email))
-print(auth.create_session("unknown@email.com"))
+
+def log_in_wrong_password(email: str, password: str) -> None:
+    """
+    test log in with wrong password functionality
+    """
+    pass
+
+
+def log_in(email: str, password: str) -> str:
+    """
+    test login functionality
+    """
+    pass
+
+
+def profile_unlogged() -> None:
+    """
+    test profile unlogged
+    """
+    pass
+
+
+def profile_logged(session_id: str) -> None:
+    """
+    test profile logged
+    """
+    pass
+
+
+def log_out(session_id: str) -> None:
+    """
+    test logout
+    """
+    pass
+
+
+def reset_password_token(email: str) -> str:
+    """
+    test reset_password functionality
+    """
+    pass
+
+
+def update_password(
+        email: str, reset_token: str, new_password: str) -> None:
+    """
+    test update password functionality
+    """
+    pass
+
+
+EMAIL = "guillaume@holberton.io"
+PASSWD = "b4l0u"
+NEW_PASSWD = "t4rt1fl3tt3"
+
+
+if __name__ == "__main__":
+
+    register_user(EMAIL, PASSWD)
+    log_in_wrong_password(EMAIL, NEW_PASSWD)
+    profile_unlogged()
+    session_id = log_in(EMAIL, PASSWD)
+    profile_logged(session_id)
+    log_out(session_id)
+    reset_token = reset_password_token(EMAIL)
+    update_password(EMAIL, reset_token, NEW_PASSWD)
+    log_in(EMAIL, NEW_PASSWD)
