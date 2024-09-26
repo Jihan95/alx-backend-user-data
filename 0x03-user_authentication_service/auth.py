c6a8c70f3796f07ed162d214408cbdad532be6ac
jihan ahmed <jihanahmed41@gmail.com>
2024-09-26 07:12:47 +0000

    12. Find user by session ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -73,7 +73,7 @@ class Auth:
         except Exception:
             return None
 
-    def get_user_from_session_id(session_id: str) -> Optional(User):
+    def get_user_from_session_id(session_id: str) -> Union[User, None]:
         """
         get user from session id
         """
