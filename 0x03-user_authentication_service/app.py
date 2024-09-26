7f323d82b4c934434e352d711101e0a02065c23c
jihan ahmed <jihanahmed41@gmail.com>
2024-09-26 07:58:13 +0000

    15. User profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -60,5 +60,17 @@ def logout():
     return redirect('/')
 
 
+@app.route("/profile", methods=['GET'])
+def profile():
+    """
+    user profile
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    return jsonify({"email": f"{user.email}"}, 200)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
